ea4ed00c1558c13c90412602b481c7140ce2ac81
Janis Loeffel <loeffelj@bzz.ch>
2022-04-26 11:45:52 +0200

    javascript add html elements for every image

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -6,7 +6,7 @@ function showImageFull(input) {
     var img = document.getElementById('myImg0' + input);
     var modalImg = document.getElementById("modalimg0" + input);
     var captionText = document.getElementById("caption0" + input);
-    img.onclick = function(){
+    img.onclick = function() {
         modal.style.display = "block";
         modalImg.src = this.src;
         modalImg.alt = this.alt;
@@ -14,14 +14,72 @@ function showImageFull(input) {
     }
 
     // Get the <span> element that closes the modal
-    var span = document.getElementById("close0" + input)[0];
-    console.log(span)
+    var span = document.getElementById("close0" + input);
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-        console.log("clicked")
         modal.style.display = "none";
     }
+}
+
+function addImages(imageList) {
+    var counter = 0;
+    $.each(imageList, function (index, image) {
+        counter++
+        let imageGallery = document.getElementById("imageGallery")
+        let htmlDiv = document.createElement("div")
+        htmlDiv.setAttribute("class", "img")
+
+        //add modal trigger element
+        let imageElement = document.createElement("img")
+        imageElement.setAttribute("onload", "showImageFull(" + counter + ")")
+        imageElement.setAttribute("id", "myImg0" + counter)
+        imageElement.setAttribute("alt", "metadata")
+        imageElement.setAttribute("src", image.path)
+        htmlDiv.appendChild(imageElement)
+
+        //add modal element
+        let modalElement = document.createElement("div")
+        modalElement.setAttribute("id", "myModal0" + counter)
+        modalElement.setAttribute("class", "modal")
+        let spanElement = document.createElement("span")
+        spanElement.setAttribute("class", "close")
+        spanElement.setAttribute("id", "close0" + counter)
+        spanElement.innerHTML = "×"
+        let imgElement = document.createElement("img")
+        imgElement.setAttribute("class", "modal-content")
+        imgElement.setAttribute("id", "modalimg0" + counter)
+        let divElement = document.createElement("div")
+        divElement.setAttribute("class", "caption")
+        divElement.setAttribute("id", "caption0" + counter)
+
+
+        modalElement.appendChild(spanElement)
+        modalElement.appendChild(imgElement)
+        modalElement.appendChild(divElement)
+        htmlDiv.appendChild(modalElement)
+        imageGallery.appendChild(htmlDiv)
+    });
+}
+
+function loadImages() {
+    var waiting = $.Deferred();
+    $
+        .ajax({
+            url: "./images",
+            dataType: "json",
+            type: "GET"
+        })
+        .done(addImages)
+        .fail(function (xhr, status, errorThrown) {
+            if (xhr.status == 404) {
+                showMessage("error","Kein Image gefunden");
+            } else {
+                window.location.href = "./";
+            }
+        })
+
+    return waiting;
 }
 
 function readURL(input) {
